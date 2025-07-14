9301018ec7b2d2e7554807e2f94bf7923182cffb
flussiger <torbenemmel@gmail.com>
2025-07-14 14:50:06 +0000

    test3

--- src/Weather.cpp
+++
@@ -1,9 +0,0 @@
-#include "Weather.h"
-#include <iostream>
-
-Weather::Weather(const std::string& city, double temp)
-    : city_(city), temp_(temp) {}
-
-void Weather::print() const {
-    std::cout << "Stadt: " << city_ << ", Temperatur: " << temp_ << "°C\n";
-}
